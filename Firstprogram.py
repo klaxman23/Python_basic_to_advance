0c4e4c490ba97ee1558741f46a9b4e9e887d226d
Laxman <laxmangbs1@gmail.com>
2024-08-10 00:00:49 +0530

    This is first program

---
+++ Firstprogram.py
@@ -0,0 +1,5 @@
+#Write a program to print the hello word program.
+
+print("Hello World")
+print("Python was created by Guido van Rossum and  was first released in 1991")
+print("Python invented by Guido van Rossum")
