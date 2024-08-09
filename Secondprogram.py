cd89322066db9759958cba3efcf68beafb1f892b
Laxman <laxmangbs1@gmail.com>
2024-08-10 00:16:39 +0530

    Write a program to print differnt data type

---
+++ Secondprogram.py
@@ -0,0 +1,15 @@
+#Write a program to print differnt data type.
+
+name = "Raj Kumar"
+age = 21
+Total_marks = 955.98
+
+print("Student name :",name)
+print("Student age :",age)
+print("Student marks :",Total_marks)
+
+print(type(name))
+print(type(age))
+print(type(Total_marks))
+
+
