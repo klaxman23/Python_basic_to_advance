d0b8e3fba5f3e35f538150cb65cd9e0ae4541196
Laxman <laxmangbs1@gmail.com>
2024-08-17 15:39:14 +0530

    Python Program to Add Two Numbers

---
+++ Sixthprogram.py
@@ -0,0 +1,11 @@
+#1.  Python Program to Add Two Numbers
+
+num = int(input("Enter a value:" ))
+num1 = int(input("Enter a value:" ))
+
+num2 = num + num1
+print(f"{num} Addition {num1} : result {num1+num}")
+
+
+
+
