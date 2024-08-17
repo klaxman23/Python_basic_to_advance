26a8f492e6a03a1a791188a9ba2f30bdced04dd3
Laxman <laxmangbs1@gmail.com>
2024-08-17 13:49:24 +0530

    Simple class program

---
+++ Fifthprogram.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John",26)
+p2 = Person("Mohan",21)
+print("Person name and age",p1.name,p1.age)
+print("Person name and age",p2.name,p2.age)
+    
+for i in p1.name:
+    print(i)
+for i in p2.name:
+    print(i)
--- Thirdprogram.py
+++ Thirdprogram.py
@@ -6,3 +6,64 @@ print(2*txt*3)
 a,b ="2",3
 txt = "#"
 print((a+txt)*b)
+
+##############
+a = 3
+b = 4
+c = 5
+print(a+b*c)
+###########################
+a = 99.0
+b = 1
+print(a+b)
+
+#############
+
+a = 1
+b = 2
+print(a/b)
+
+##########################
+
+a = 3.0
+b = 3
+print(a//b)
+
+######################
+
+a = 12
+b = -5
+print(a//b)
+
+
+##########################
+
+name  = "Hello All good morning\ti am fine"
+print(name)
+
+#####################
+
+mnmn = "Hello" + "World"
+
+print(mnmn)
+
+##################
+
+n = "india"
+state = "Hyderbad"
+
+print(len(n))
+print(len(state))
+
+##########################
+
+class Goat:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+        
+p1 = Goat("John",23)
+
+print(p1.name)
+print(p1.age)
+
