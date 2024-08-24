d9ffe7854504b87b55428fa1bd05c47ed2d92bc2
Laxman <laxmangbs1@gmail.com>
2024-08-24 20:55:33 +0530

    New program added

--- def-example.py
+++ def-example.py
@@ -1,3 +1,16 @@
+#Write a program with use of defination in python.
 def my_fun():
     print("My name is Laxman learning python and FastAPI")
 my_fun()
+
+# Write a python program with use def key.
+
+def my_fun_details(fname,lname):
+    print(fname + " " + lname)
+
+
+my_fun_details("Kadhambala","Laxman")
+my_fun_details("Kadhambala","Suma")
+my_fun_details("Kadhambala","Kavya")
+my_fun_details("Kadhambala","Nihal")  
+
