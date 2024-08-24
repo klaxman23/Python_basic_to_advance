908fb7d710b26159caf0cb6fe86e463efab15347
Laxman <laxmangbs1@gmail.com>
2024-08-24 22:42:04 +0530

    new lines of code added

--- def-example.py
+++ def-example.py
@@ -20,7 +20,44 @@ def my_fun_details(fname):
     print(fname +" " + "Laxman")
     
 my_fun_details("Kadhambala")
-    
+my_fun_details("Suma")
 
+#Arbitrary Keyword Arguments, **kwargs
+
+def my_new_function(**kid):
+    print("His name is "+ kid["lname"])
+    
+my_new_function(fname = "Nemalapuri" , lname = "Sathish")
+
+print("/"*100)
+
+#Passing a List as an Argument
+
+def my_function_new(food):
+    for i in food:
+        print(i)
+    
+fruits = ["Apple","Orange","Grapes"]
+grosers = ["Atta","Maida","Rice"]
+my_function_new(fruits)
+my_function_new(grosers)
+
+print("@"*20)
+#Adding two numbers
+
+def add(x,y):
+    return x + y
+
+result = add(25,65)
+
+print("Adding two number result:",result)
+
+print("$"*25)
+
+#Return value.
+def my_funtion(x):
+    return 5 * x
+
+print("Result:",my_funtion(3))
 
 
